f4798dba8ad48796cdedd35b6e40dfea0271f5f6
favabb <buriolfavb@gmail.com>
2026-06-13 21:28:17 -0300

    ListaPajeros

--- Obl-AED-mar26/src/tads/ListaPasajeros.java
+++ Obl-AED-mar26/src/tads/ListaPasajeros.java
@@ -4,10 +4,25 @@
  */
 package tads;
 
+import dominio.Pasajero;
+
 /**
  *
  * @author burio
  */
 public class ListaPasajeros {
+    private ILista<Pasajero> listaDePasajeros;
+
     
+    public ListaPasajeros() {
+        this.listaDePasajeros = new Lista<>(); 
+    }
+
+    public void registrarPasajero(Pasajero pasajero) {
+
+    }
+    
+    public Pasajero buscarPasajero (String cedula) {
+        
+    }
 }
