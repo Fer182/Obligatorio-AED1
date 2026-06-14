5f85e114f6ad313c6b901ad0383bcbba1a058c38
FERAR <FERAR@DESKTOP-DN58GBV>
2026-06-13 22:11:40 -0300

    Agrego comparable, y ToString

---
+++ Obl-AED-mar26/src/dominio/PasajeroWrapper.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author FERAR
+ */
+public class PasajeroWrapper {
+    private Pasajero pasajero;
+    
+    public PasajeroWrapper(Pasajero p){
+    this.pasajero = p;
+    }
+}
